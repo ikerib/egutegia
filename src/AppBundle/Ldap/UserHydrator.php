95e7396316410e7259b8a945db5f9dbe10446946
Iker Ibarguren <ikerib@gmail.com>
2017-03-24 14:20:57 +0100

    Set ROLE_ADMIN to LDAP Administrator group member users

--- src/AppBundle/Ldap/UserHydrator.php
+++ src/AppBundle/Ldap/UserHydrator.php
@@ -12,8 +12,8 @@ use AppBundle\Entity\User;
 use FR3D\LdapBundle\Hydrator\HydratorInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-class UserHydrator implements HydratorInterface
-{
+class UserHydrator implements HydratorInterface {
+
     /**
      * Populate an user with the data retrieved from LDAP.
      *
@@ -22,25 +22,35 @@ class UserHydrator implements HydratorInterface
      *
      * @return UserInterface
      */
-    public function hydrate(array $ldapEntry)
+    public function hydrate (array $ldapEntry)
     {
         $user = new User();
-        $user->setUsername($ldapEntry['samaccountname'][0]);
-        $user->setEmail($ldapEntry['mail'][0]);
-        if (  (array_key_exists('department', $ldapEntry)) && (count($ldapEntry[ 'department' ]))) {
+        $user->setUsername($ldapEntry[ 'samaccountname' ][ 0 ]);
+        $user->setEmail($ldapEntry[ 'mail' ][ 0 ]);
+        if (( array_key_exists('department', $ldapEntry) ) && ( count($ldapEntry[ 'department' ]) )) {
             $user->setDepartment($ldapEntry[ 'department' ][ 0 ]);
         }
-        if ((array_key_exists('employeeid', $ldapEntry)) && (count($ldapEntry[ 'employeeid' ]))) {
-            $user->setNan($ldapEntry['employeeid'][0]);
+        if (( array_key_exists('employeeid', $ldapEntry) ) && ( count($ldapEntry[ 'employeeid' ]) )) {
+            $user->setNan($ldapEntry[ 'employeeid' ][ 0 ]);
         }
-        if ((array_key_exists('description', $ldapEntry)) && (count($ldapEntry[ 'description' ]))) {
-            $user->setLanpostua($ldapEntry['description'][0]);
+        if (( array_key_exists('description', $ldapEntry) ) && ( count($ldapEntry[ 'description' ]) )) {
+            $user->setLanpostua($ldapEntry[ 'description' ][ 0 ]);
         }
-        if ((array_key_exists('displayname', $ldapEntry)) && (count($ldapEntry[ 'displayname' ]))) {
-            $user->setDisplayname($ldapEntry['displayname'][0]);
+        if (( array_key_exists('displayname', $ldapEntry) ) && ( count($ldapEntry[ 'displayname' ]) )) {
+            $user->setDisplayname($ldapEntry[ 'displayname' ][ 0 ]);
         }
-        if ((array_key_exists('memberof', $ldapEntry)) && (count($ldapEntry[ 'memberof' ]))) {
-            $user->setMembers($ldapEntry['memberof']);
+        if (( array_key_exists('memberof', $ldapEntry) ) && ( count($ldapEntry[ 'memberof' ]) )) {
+            $members = $ldapEntry[ 'memberof' ];
+            $rol = "ROLE_USER";
+            foreach ($members as $key => $value) {
+
+                $sp = ldap_explode_dn($value,1);
+                if ( $sp[0] == "InformatikaSaila") {
+                    $rol = "ROLE_ADMIN";
+                }
+            }
+            $user->setMembers($ldapEntry[ 'memberof' ]);
+            $user->addRole('ROLE_ADMIN');
         }
         $user->setDn($ldapEntry[ 'dn' ]);
         $user->setEnabled(true);
