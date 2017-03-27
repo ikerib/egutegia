92c81253f6afc81628232183bbb1ef0b78313535
Iker Ibarguren <ikerib@gmail.com>
2017-03-27 10:09:34 +0200

    Login form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,30 +4,21 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function homepageAction()
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $ldap = $this->get('ldap_tools.ldap_manager');
-        $ldapusers = $ldap->buildLdapQuery()->fromUsers()->orderBy('username')->getLdapQuery()->getResult();
-
-        $userdata=[];
-        foreach ($ldapusers as $user) {
-            $u = [];
-            $u["user"] = $user;
-            $calendar = $em->getRepository('AppBundle:Calendar')->findByUsername($user->getUsername());
-            $u[ "calendar" ] = $calendar;
-            array_push($userdata, $u);
-        }
-
-
-        return $this->render('default/index.html.twig', array('userdata'=>$userdata));
+        $user = $this->getUser();
+        return $this->render('default/homepage.html.twig', array(
+            'user' => $user,
+        ));
     }
+
 }
