6b800a9190eec16e3b737aa60d6d5e67d3d4bf6a
Iker Ibarguren <ikerib@gmail.com>
2017-03-29 11:09:33 +0200

    Menu with user/role info

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -8,103 +8,102 @@
 
 namespace AppBundle\Menu;
 
+use AppBundle\AppBundle;
+use AppBundle\Entity\User;
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
+class Builder implements ContainerAwareInterface {
 
-class Builder implements ContainerAwareInterface
-{
     use ContainerAwareTrait;
 
-    public function mainMenu(FactoryInterface $factory, array $options)
+
+    public function mainMenu (FactoryInterface $factory, array $options)
     {
-        $em = $this->container->get('doctrine')->getManager();
-        $menu = $factory->createItem('root', array(
-            'navbar' => true,
-        ));
+        $menu = $factory->createItem('root',array('navbar' => true,));
 
-        $menu->addChild('Hasiera', array(
-            'icon'  => 'home',
-            'route' => 'dashboard',
-        ));
+        $menu->addChild('Hasiera',array('icon'  => 'home','route' => 'dashboard',));
 
-        $dropdown = $menu->addChild('Txantiloiak', array(
-            'dropdown' => true,
-            'caret'    => true,
-        ));
-        $dropdown->addChild('Zerrenda', array('route' => 'admin_template_index'));
-        $dropdown->addChild('Berria', array('route' => 'admin_template_new'));
+        $dropdown = $menu->addChild('Txantiloiak',array('dropdown' => true,'caret'    => true,));
+        $dropdown->addChild('Zerrenda', array( 'route' => 'admin_template_index' ));
+        $dropdown->addChild('Berria', array( 'route' => 'admin_template_new' ));
 
-        $menu->addChild('Motak', array(
-            'dropdown' => true,
-            'caret'    => true,
-        ));
-        $menu['Motak']->addChild('Zerrenda', array('route' => 'admin_type_index'));
-        $menu['Motak']->addChild('Berria', array('route' => 'admin_type_new'));
+        $menu->addChild(
+            'Motak',
+            array(
+                'dropdown' => true,
+                'caret'    => true,
+            )
+        );
+        $menu[ 'Motak' ]->addChild('Zerrenda', array( 'route' => 'admin_type_index' ));
+        $menu[ 'Motak' ]->addChild('Berria', array( 'route' => 'admin_type_new' ));
 
+        return $menu;
+    }
 
+    public function userMenu (FactoryInterface $factory, array $options) {
 
+        $checker = $this->container->get('security.authorization_checker');
+        /** @var $user User */
+        $user = $this->container->get('security.token_storage')->getToken()->getUser();
 
-        $menu->addChild('User', array('label' => 'Kaixo user'))
-            ->setAttribute('dropdown', true)
-            ->setAttribute('icon', 'fa fa-user');
-        $menu['User']->addChild('Edit profile', array('route' => 'homepage'))
-            ->setAttribute('icon', 'fa fa-edit');
+        $menu = $factory->createItem('root',array('navbar' => true,'icon'=>'glyphicon glyphicon-user'));
 
+        if ($checker->isGranted('ROLE_USER') || ($checker->isGranted('ROLE_ADMIN'))) {
+            $menu->addChild('User', array( 'label' => $user->getDisplayname() ))
+                ->setAttribute('dropdown', true)
+                ->setAttribute('icon', 'glyphicon glyphicon-user');
 
-        $sc = $this->container->get('security.context');
-
-        if($sc->getToken())
-        {
-            $menu->addChild('Members', array('uri' => '#'))
-                ->setAttribute('dropdown', true);
-
-            if($sc->isGranted('IS_AUTHENTICATED_REMEMBERED'))
-            {
-                $menu['Members']->addChild('My profile', array('route' => 'fos_user_profile_show'));
-
-                if($sc->isGranted('ROLE_ADMIN'))
-                {
-                    $menu['Members']->addChild('Dashboard', array('route' => 'sonata_admin_dashboard'))
-                        ->setAttribute('divider_append', true);
-                }
-
-                $menu['Members']->addChild('Logout', array('route' => 'fos_user_security_logout'));
+            if ($checker->isGranted('ROLE_ADMIN')) {
+                $menu[ 'User' ]->addChild('Egutegia', array( 'route' => 'homepage' ))
+                    ->setAttribute('icon', 'fa fa-edit');
+            } else {
+                $menu[ 'User' ]->addChild('Egutegiak', array( 'route' => 'dashboard' ))
+                    ->setAttribute('icon', 'fa fa-edit');
             }
-            else
-            {
-                $menu['Members']->addChild('Login', array('route' => 'fos_user_security_login'));
-                $menu['Members']->addChild('Registration', array('route' => 'fos_user_registration_register'));
-            }
+
+            $menu['User']->addChild('Irten', array('route' => 'fos_user_security_logout'));
+        } else {
+            $menu->addChild('login', array('route' => 'fos_user_security_login'));
         }
 
         return $menu;
+
     }
 
-    public function subMenuLeft(FactoryInterface $factory, array $options)
+    public function subMenuLeft (FactoryInterface $factory, array $options)
     {
-        $menu = $factory->createItem('root', array(
-            'navbar' => true,
-        ));
+        $menu = $factory->createItem(
+            'root',
+            array(
+                'navbar' => true,
+            )
+        );
         $menu->setChildrenAttribute('class', 'navbar navbar-default navbar-lower affix-top');
-        $menu->addChild('Txantiloiak', array('uri' => 'javascript:void(0);'));
-        
+        $menu->addChild('Txantiloiak', array( 'uri' => 'javascript:void(0);' ));
+
         return $menu;
     }
 
-    public function subMenuRight(FactoryInterface $factory, array $options)
+    public function subMenuRight (FactoryInterface $factory, array $options)
     {
-        $menu = $factory->createItem('root', array(
-            'navbar' => true,
-        ));
-        $menu->setChildrenAttribute('class', 'navbar navbar-default navbar-lower affix-top');
-        $menu->addChild('Grabatu', array(
-            'attributes' => array(
-                'id' => 'btnGrabatu',
-                'class' => 'btn btn-primary navbar-btn'
+        $menu = $factory->createItem(
+            'root',
+            array(
+                'navbar' => true,
             )
-        ));
+        );
+        $menu->setChildrenAttribute('class', 'navbar navbar-default navbar-lower affix-top');
+        $menu->addChild(
+            'Grabatu',
+            array(
+                'attributes' => array(
+                    'id'    => 'btnGrabatu',
+                    'class' => 'btn btn-primary navbar-btn',
+                ),
+            )
+        );
 
 
         return $menu;
